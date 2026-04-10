1b5d97db3aa3f98af56874c934b26cb5a761126f
LazuardiMahendra <lazuardhie.mahendra@gmail.com>
2026-04-10 21:27:03 +0800

    Initial Commit

---
+++ Find My Age 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Find My Age 2
+//
+//  Created by Lazuardi Mahendra on 4/10/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
